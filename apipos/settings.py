327f05cfb5ba0bc2c9de36bc4daa03e6a010771c
jorge Lenovo <jorge.montagut@gmail.com>
2024-07-19 15:56:52 -0500

    para producción

--- apipos/settings.py
+++ apipos/settings.py
@@ -100,8 +100,8 @@ WSGI_APPLICATION = 'apipos.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        default='postgresql://postgres:postgres@localhost:5432/apiPOS',
+        conn_max_age=600    
     )
 }
 
